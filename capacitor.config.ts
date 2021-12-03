161becf7cc73bcf56df628cd2edfc034e4355074
Joel Henry <joel.henry@unifii.com.au>
2021-12-03 11:08:40 +1100

    bug

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'text-zoom-bug',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { TextZoom } from '@capacitor/text-zoom';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
   title = 'text-zoom-bug';
+
+  constructor() {
+    this.setTextZoom(100);
+  }
+
+
+  async setTextZoom(percentage: number) {
+    if (!(Capacitor.getPlatform() !== 'web' && Capacitor.isPluginAvailable('text-zoom'))) {
+      console.log('web, do not use textzoom!');
+      return;
+    }
+
+    await TextZoom.set({ value: percentage / 100 });
+  }
+
 }
